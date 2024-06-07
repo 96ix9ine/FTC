652213890e74fef396477877e492a85957883d2b
96ix9ine <sergstar2012@mail.ru>
2024-06-07 17:11:18 +0500

    added registration window for all buttons type REGISTRATION

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,23 +1,33 @@
 var modal = document.getElementById("registration");
-
-var btn = document.getElementsByClassName("button-reg")[0];
-
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
-    document.body.classList.add("modal-open");
-  }
-  
-  span.onclick = function() {
+function openModal() {
+  modal.style.display = "block";
+  document.body.classList.add("modal-open");
+  anime({
+    targets: '.modal-content',
+    scale: [0.7, 1],
+    duration: 500,
+    easing: 'easeInOutQuad'
+  });
+}
+
+var buttons = document.querySelectorAll('.button-reg');
+buttons.forEach(button => {
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    openModal();
+  });
+});
+
+span.onclick = function() {
+  modal.style.display = "none";
+  document.body.classList.remove("modal-open");
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.display = "none";
     document.body.classList.remove("modal-open");
   }
-  
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-      document.body.classList.remove("modal-open");
-    }
-  }
-
+}
