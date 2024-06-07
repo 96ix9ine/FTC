3a015d09cc06401e423840ca6d93c1a40c539ed2
96ix9ine <sergstar2012@mail.ru>
2024-06-07 16:57:29 +0500

    minor fixes

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,31 +1,23 @@
-// JavaScript to control the modal
-// Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("registration");
 
-// Get the button that opens the modal
 var btn = document.getElementsByClassName("button-reg")[0];
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-// Когда пользователь кликает по кнопке, открываем модальное окно
 btn.onclick = function() {
     modal.style.display = "block";
-    document.body.classList.add("modal-open"); // Добавляем класс для предотвращения прокрутки
+    document.body.classList.add("modal-open");
   }
   
-  // Когда пользователь кликает на <span> (x), закрываем модальное окно
   span.onclick = function() {
     modal.style.display = "none";
-    document.body.classList.remove("modal-open"); // Удаляем класс для разрешения прокрутки
+    document.body.classList.remove("modal-open");
   }
   
-  // Когда пользователь кликает где-то за пределами модального окна, закрываем его
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
-      document.body.classList.remove("modal-open"); // Удаляем класс для разрешения прокрутки
+      document.body.classList.remove("modal-open");
     }
   }
 
